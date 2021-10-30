927d2817795b34e0467732566b1604a1d2977e1c
thTs0ustas <th.ts0ustas@gmail.com>
2021-10-30 16:18:37 +0300

    xmasTree probably finished

--- xmasTree/xmasTreeScript.js
+++ xmasTree/xmasTreeScript.js
@@ -1,37 +1,57 @@
 function xmasTree(height) {
-  return upperXmasTree(height); // bottomXmasTree(height);
+  return `${upperXmasTree(height)}${bottomXmasTree(height)}`;
 }
 
 function upperXmasTree(height) {
   let upperStars = "";
   let topStar = xmasTreeTopStar(height);
-  for (let i = 1; i < height - 1; i++) {
+  for (let i = 1; i < height; i++) {
     if (i === 1) {
       upperStars += topStar.replace(replacementStarsAndSpaces(i), starsToAdd(i));
     } else {
       upperStars += "\n" + topStar.replace(replacementStarsAndSpaces(i), starsToAdd(i));
     }
-    // console.log(upperStars);
   }
   return upperStars;
 }
 
-// function bottomXmasTree(height) {
-//   let bottomStart = "";
-//   let topStar = xmasTreeTopStar(height);
-//   for (let i = height; i > height / 2; i--) {
-//     bottomStart += "\n" + topStar.replace(replacementStarsAndSpaces(i), starsToAdd(i));
-//   }
-//
-//   return bottomStart;
-// }
+function bottomXmasTree(height) {
+  let bottomStart = "";
+  let topStar = xmasTreeTopStar(height);
+  let iterator = height;
+  let heightDivider;
+  if (height > 10) {
+    heightDivider = 5;
+  } else if (height >= 5 && height <= 10) {
+    heightDivider = 4;
+  } else {
+    heightDivider = 3;
+  }
+
+  // create the bottom part of the tree
+
+  for (let j = iterator; j > 0; j -= height / heightDivider + 1) {
+    if (j !== iterator) {
+      bottomStart += "\n" + topStar.replace(replacementStarsAndSpaces(j), starsToAdd(j));
+    }
+  }
+
+  // create the tree trunk
+
+  let treeTrunk = bottomStart.split("\n")[bottomStart.split("\n").length - 1];
+
+  for (let i = 0; i < height / 5; i++) {
+    bottomStart += "\n" + treeTrunk;
+  }
+  return bottomStart;
+}
 
 function xmasTreeTopStar(height) {
   let xmasTreeStar = " *";
   let xmasTreeSpace = " ";
   let topTreeStar = "";
 
-  for (let rowStar = 1; rowStar < height * 2 - 1; rowStar++) {
+  for (let rowStar = 1; rowStar < height * 2; rowStar++) {
     rowStar % height === 0
       ? (topTreeStar += xmasTreeStar)
       : (topTreeStar += xmasTreeSpace);
@@ -53,7 +73,7 @@ function starsToAdd(i) {
 
 function replacementStarsAndSpaces(i) {
   let replacement = "";
-  for (let j = 0; j < i; j++) {
+  for (let j = 1; j < i; j++) {
     replacement += " ";
   }
 
@@ -65,14 +85,4 @@ function replacementStarsAndSpaces(i) {
   return replacement;
 }
 
-console.log(xmasTree(20));
-
-// function factorial(num) {
-//   if (num !== 0) {
-//     return num * factorial(num - 1);
-//   } else {
-//     return 1;
-//   }
-// }
-
-// console.log(factorial(5));
+console.log(xmasTree(69));
