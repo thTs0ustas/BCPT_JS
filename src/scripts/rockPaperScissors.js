1e1380cc74f362a8baedf51131a194b58d916c89
thTs0ustas <th.ts0ustas@gmail.com>
2021-11-28 17:43:07 +0200

    oop

---
+++ src/scripts/rockPaperScissors.js
@@ -0,0 +1,36 @@
+const chooseTheWinner = (player, computer) => {
+  if (
+    (player === "rock" && computer === "paper") ||
+    (player === "scissor" && computer === "rock") ||
+    (player === "paper" && computer === "scissors")
+  )
+    return "Computer wins";
+  else if (player === computer) return "Tie";
+  else return "Player wins";
+};
+
+const rockPaperScissors = () => {
+  const possibleOptions = ["rock", "paper", "scissors"];
+
+  let winner = "";
+  let games = 0;
+  let computer = "";
+
+  while (games < 2) {
+    computer = possibleOptions[Math.floor(Math.random() * 3)];
+    let player = prompt("Select your  hand");
+
+    while (!possibleOptions.includes(player)) {
+      console.error("Only rock paper or scissors");
+      player = prompt("Select your  hand");
+    }
+    console.log(`Player: ${player} - Computer: ${computer}`);
+
+    winner = chooseTheWinner(player, computer);
+    games++;
+
+    console.log(winner);
+  }
+};
+
+rockPaperScissors();
